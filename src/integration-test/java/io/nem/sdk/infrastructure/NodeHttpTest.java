08a31042739f5a94f51ea9401154d13e0b3ab6ee
Steven Liu <xian.f.liu@gmail.com>
2019-06-24 21:58:40 +0100

    Added NodeHttp

---
+++ src/integration-test/java/io/nem/sdk/infrastructure/NodeHttpTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 NEM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.nem.sdk.infrastructure;
+
+import io.nem.sdk.model.node.NodeInfo;
+import io.nem.sdk.model.node.NodeTime;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class NodeHttpTest extends BaseTest {
+    private NodeHttp nodeHttp;
+
+    @BeforeAll
+    void setup() throws IOException {
+        nodeHttp = new NodeHttp(this.getApiUrl());
+    }
+
+    @Test
+    void getNodeInfo() throws ExecutionException, InterruptedException {
+        NodeInfo nodeInfo = nodeHttp
+                .getNodeInfo()
+                .toFuture()
+                .get();
+
+        assertTrue(nodeInfo.getPublicKey() != "");
+        assertTrue(nodeInfo.getHost() != null);
+        assertTrue(nodeInfo.getPort() > 0);
+        assertTrue(nodeInfo.getNetworkIdentifier().getValue() > 0);
+    }
+
+    @Test
+    void getNodeTime() throws ExecutionException, InterruptedException {
+        NodeTime nodeTime = nodeHttp
+                .getNodeTime()
+                .toFuture()
+                .get();
+
+        assertTrue(nodeTime.getReceiveTimeStamp().longValue() > 0);
+        assertTrue(nodeTime.getSendTimeStamp().longValue() > 0);
+    }
+}
