46496bde4192494a2862a5546c25ed11be533e52
Fernando Boucquez <fboucquez@gmail.com>
2019-10-15 23:32:06 +0200

    conditional release

--- release.sh
+++ release.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Releasing sdk jars and javadocs"
-./gradlew release uploadArchives publishGhPages
 
-git push
+if [ "$RELEASE_ENABLED" = "true" ]
+then
+  echo "Releasing sdk jars and javadocs"
+  ./gradlew release uploadArchives publishGhPages
+  git push
+else
+  echo "Release is disabled"
+fi
