7836d6d2d89c3d2c9f81f553d776312cbb13ce59
Fernando Boucquez <fboucquez@gmail.com>
2019-10-15 22:06:18 +0200

    Release v0.7.19
    Prepare next release v0.7.20-SNAPSHOT

--- release.sh
+++ release.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Uploading generated clients jars and javadocs"
-./gradlew -b ./openapi-generator/build.gradle release uploadArchives publishGhPages
-
-echo "Uploading sdk jars and javadocs"
+echo "Releasing sdk jars and javadocs"
 ./gradlew release uploadArchives publishGhPages
 
 git push
