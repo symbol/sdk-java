3d990f2e7dabf776b73cfa0a5d749c33a1a058ce
Fernando Boucquez <fboucquez@gmail.com>
2019-10-24 17:45:43 +0200

    moved travis files into it's own folder

--- travis/build.sh
+++ travis/build.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Current branch is: '$TRAVIS_BRANCH'. Release branch is '$RELEASE_BRANCH'"
 
-if [ "$TRAVIS_BRANCH" = "$RELEASE_BRANCH" ]
+if [ "$TRAVIS_BRANCH" != "" ] && [ "$TRAVIS_BRANCH" = "$RELEASE_BRANCH" ]
 then
   echo "Currently releasing in branch '$TRAVIS_BRANCH'. Normal build is ignored"
 else
